dcfb8f5a8709666c9383ffc5938c9ca4df66a45d
wuyunzhemu <415159489@qq.com>
2020-01-23 01:14:31 +0800

    抽离播放状态为共享状态

--- src/components/playerTab/playerTab.js
+++ src/components/playerTab/playerTab.js
@@ -1,21 +1,18 @@
-import React, { useState, createRef,useContext } from 'react';
+import React, { useState, createRef,useContext, useEffect } from 'react';
 import './playerTab.css'
 import playList from '../../store/playListState/index';
+import playStatus from '../../store/playStatusState/index';
 import ProgressBar from './progressBar';
 
 const PlayerTab = (props) => {
-  const [onPlaying, setOnPlaying] = useState(false);
-  const {state,dispatch:listDispatch} = useContext(playList.playListContext);
-  const curSong = state.songsList[state.curSongIndex];
-  // console.log(state);
-  const musicPlayer = createRef();
-
-  //更改播放状态 播放/暂停
-  const changePlayStatus = () => {
-    // const player = this.refs.musicPlayer;
+  const {state:statusState,dispatch:statusDispatch } = useContext(playStatus.playStatusContext)
+  const {state:listState,dispatch:listDispatch} = useContext(playList.playListContext);
+  const curSong = listState.songsList[listState.curSongIndex]; // 当前歌曲obj
+  const onPlaying = statusState.onPlaying; // 当前播放状态:boolean
+  useEffect(()=>{
     if (musicPlayer.current) {
       const player = musicPlayer.current; //播放器dom
-      if (player.paused) {
+      if (onPlaying) {
         player.play();
       }
       else {
@@ -23,7 +20,13 @@ const PlayerTab = (props) => {
       }
       console.log('暂停：' + player.paused);
     }
-    setOnPlaying(!onPlaying);
+  },[onPlaying]);
+  // console.log(state);
+  const musicPlayer = createRef();
+  //更改播放状态 播放/暂停
+  const changePlayStatus = () => {
+    console.log("修改播放状态");
+    statusDispatch({type:"CHANGECURSTATUS"});
   }
 
   //切换当前播放歌曲 上一曲/下一曲
@@ -42,7 +45,7 @@ const PlayerTab = (props) => {
 
   return <div className='playerTab'>
     <audio ref={musicPlayer} autoPlay={onPlaying} src={`https://music.163.com/song/media/outer/url?id=${curSong.id}.mp3`} />
-    <ProgressBar  onPlaying={onPlaying} changeCurSong={changeCurSong} duration={curSong.duration} curSongIndex={state.curSongIndex} getPlayTime={getCurrentTime}/>
+    <ProgressBar  onPlaying={onPlaying} changeCurSong={changeCurSong} duration={curSong.duration} curSongIndex={listState.curSongIndex} getPlayTime={getCurrentTime}/>
     <div className='player-button'>
       <svg t="1574175081645" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6830" width=".40rem" height=".40rem"><path d="M84.21 273.16h59.57c86.85 0 162.98 46.56 204.87 116 3.9 6.47 10.7 10.62 18.25 10.62 16.55 0 26.52-18.11 17.99-32.29-49.2-81.78-138.72-136.54-241.12-136.54H84.21c-11.65 0-21.1 9.45-21.1 21.1 0 11.66 9.45 21.11 21.1 21.11z m862.6 468.13L751.53 628.55c-18.06-10.43-40.64 2.61-40.64 23.47v99.48h-9.42c-86.85 0-162.98-46.56-204.87-116-3.9-6.47-10.7-10.62-18.25-10.62-16.55 0-26.52 18.11-17.99 32.29 49.2 81.78 138.72 136.54 241.12 136.54h9.42v83.8c0 20.86 22.58 33.9 40.64 23.47l195.27-112.74c18.06-10.45 18.06-36.52 0-46.95z" p-id="6831" fill="#ffffff"></path><path d="M710.27 273.87v97.17c0 20.86 22.58 33.9 40.64 23.47l195.27-112.74c18.07-10.43 18.07-36.5 0-46.93L750.91 122.09c-18.06-10.43-40.64 2.61-40.64 23.47v86.27c-146.67 8.97-255.16 131.52-295.79 280.5-55.29 153.52-138.82 239.17-270.7 239.17H84.21c-11.65 0-21.1 9.45-21.1 21.1s9.45 21.1 21.1 21.1h59.57c155.4 0 270.7-126.62 312.91-281.37 52.84-146.72 131.59-231.25 253.58-238.46z" p-id="6832" fill="#ffffff"></path></svg>
       <svg t="1574175126522" onClick={(e)=>{changeCurSong("PRE",e)}} style={{ marginLeft: '.6rem' }} viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7250" width=".4rem" height=".4rem"><path d="M748.43 128.5L217.52 445.78V88.17c0-12.55-10.18-22.73-22.73-22.73s-22.73 10.18-22.73 22.73v848.65c0 12.55 10.18 22.73 22.73 22.73s22.73-10.18 22.73-22.73v-357.6L748.43 896.5c46.49 27.78 105.52-5.71 105.52-59.87V188.37c0-54.16-59.04-87.65-105.52-59.87z m60.05 708.14c0 8.61-3.95 14.21-7.27 17.37-4.65 4.45-10.91 7-17.14 7-4.25 0-8.39-1.18-12.33-3.54L229.36 533.34c-10.67-6.38-11.82-16.69-11.82-20.84 0-4.14 1.15-14.46 11.82-20.84l542.39-324.13c3.94-2.35 8.08-3.54 12.33-3.54 11.74 0 24.4 9.32 24.4 24.38v648.27z" p-id="7251" fill="#ffffff"></path></svg>
