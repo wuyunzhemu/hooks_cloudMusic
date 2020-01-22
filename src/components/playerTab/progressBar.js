2033cf65c4ae2046cd93c6a4e4bf9d170f8b7e4d
wuyunzhemu <415159489@qq.com>
2020-01-23 01:13:47 +0800

    删除了进度条长度state，改为直接计算，减少刷新频率

--- src/components/playerTab/progressBar.js
+++ src/components/playerTab/progressBar.js
@@ -6,7 +6,6 @@ const ProgressBar = props => {
   // console.log(props);
   const { changeCurSong, onPlaying = false, duration = 0, curSongIndex = 0, getPlayTime } = props;
   const [curPlayTime, setCurPlayTime] = useState(0); //当前播放时间（毫秒）
-  const [playBarWidth, setPlayBarWidth] = useState(0); // 已播放进度条长度
   // console.log(curPlayTime);
 
   //计时器 播放状态时每秒更新一次当前播放位置
@@ -18,7 +17,6 @@ const ProgressBar = props => {
   //切换歌曲时 重置当前播放时长及定时器
   useEffect(() => {
     setCurPlayTime(0);
-    setPlayBarWidth(0);
     if (timer) {
       clearInterval(timer);
     }
@@ -30,15 +28,14 @@ const ProgressBar = props => {
     // 如果播放中则注册定时器，否则记录当前播放位置(防止暂停引起的一秒计时重置) 注销定时器
     if (onPlaying === true) {
       timer = setInterval(() => {
-        console.log("curTime:" + curPlayTime + "duration:" + duration);
         // 批量更新机制会导致状态重复更新为同一个值，setState(fn)则会将函数放入事件队列中，然后按顺序执行，达到更新状态的效果
         setCurPlayTime(curPlayTime => {
+          console.log(curPlayTime+" > "+ duration)
           if (curPlayTime >= duration) {
             changeCurSong("NEXT");
             clearInterval(timer);
             return;
           }
-          setPlayBarWidth(curPlayTime / duration * 100);
           return curPlayTime + PLAYINGSPEED;
         });
       }, 1000)
@@ -67,7 +64,7 @@ const ProgressBar = props => {
   return <div className="progressBar">
     <span className="timeText">{formatTime(curPlayTime)}</span>
     <div className="barWrap">
-      <div className="barInner" style={{ width: playBarWidth + '%' }}>
+      <div className="barInner" style={{ width: curPlayTime / duration * 100 + '%' }}>
         <div className="barDot"></div>
       </div>
     </div>
