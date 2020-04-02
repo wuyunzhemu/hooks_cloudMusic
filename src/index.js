ebc35297fb62f804f375d22d07caefad41a79aae
wuyunzhemu <415159489@qq.com>
2020-04-03 00:48:52 +0800

    feat:favicon

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import Player from './pages/player/player'
-ReactDOM.render(<Player/>,document.getElementById('root'));
--- src/store/providers.js
+++
@@ -1,5 +0,0 @@
-import playList from './playListState/index';
-import playStatus from './playStatusState/index';
-const providers = [playList.provider,playStatus.provider];
- 
-export default providers;
